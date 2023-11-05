1aabdd50573f12c9c4d0ce34fb9dd3eaaa6fc0ad
Pranav Shirole <95130634+shirolepranav@users.noreply.github.com>
2023-11-05 16:02:18 -0500

    Added animation to login steps

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-
+    import {page} from "$app/stores"
 </script>
 
 <nav class="flex justify-center my-6">
@@ -8,7 +8,7 @@
       <a
         href="/login/username"
         class="step"
-        class:step-primary={$page.route.id?.match(/username|photo/g)}>
+        class:step-primary={$page.route.id?.includes("username")}>
         Choose Username
       </a>
       <a
@@ -20,8 +20,11 @@
     </ul>
   </nav>
   
+  
+<AnimatedRoute>
   <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
     <div class="card-body items-center text-center">
       <slot />
     </div>
   </main>
+</AnimatedRoute>
