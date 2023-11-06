b099196021f3b211b5252febd5f9ed7d4790867e
Pranav Shirole <95130634+shirolepranav@users.noreply.github.com>
2023-11-06 06:28:00 -0500

    added ability to write multiple documents to Firestore automatically

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -30,8 +30,31 @@
     }
   
     async function confirmUsername() {
-      // TODO
-    }
+    console.log("confirming username", username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), { uid: $user?.uid });
+    batch.set(doc(db, "users", $user!.uid), { 
+      username, 
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: 'I am the Walrus',
+      links: [
+        {
+          title: 'Test Link',
+          url: 'https://kung.foo',
+          icon: 'custom'
+        }
+      ]
+    });
+
+    await batch.commit();
+
+    username = '';
+    isAvailable = false;
+
+  }
+
+</script>
   
   </script>
   
@@ -55,3 +78,6 @@
         </form>
   
   </AuthCheck>
+
+
+  
