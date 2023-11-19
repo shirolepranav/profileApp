0c778d40422c90e84f7b66133e296ffefaedc1e1
Pranav Shirole <95130634+shirolepranav@users.noreply.github.com>
2023-11-19 12:50:42 -0500

    Firebase cookie signin & singout, updated Signin buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { auth, user } from "$lib/firebase";
+  
+    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+  
+    async function signInWithGoogle() {
+      const provider = new GoogleAuthProvider();
+      const credential = await signInWithPopup(auth, provider);
+  
+      const idToken = await credential.user.getIdToken();
+  
+      const res = await fetch("/api/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+        },
+        body: JSON.stringify({ idToken }),
+      });
+    }
+  
+    async function signOutSSR() {
+      const res = await fetch("/api/signin", { method: "DELETE" });
+      await signOut(auth);
+    }
+  </script>
